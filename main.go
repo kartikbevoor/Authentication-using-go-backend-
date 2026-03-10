2732f52c88616c9c37182d5dd1dceb12a9b782a9
Kartik Bevoor <kartikbevoor2130@gmail.com>
2026-03-10 18:05:11 +0530

    First commit

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"Authentication_Using_JWT/routes"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080"
+	}
+
+	router := gin.New() // creates a new Gin router (engine) and returns a pointer to a Gin Engine.
+	// router := gin.Default()	// automatically adds middleware like: logger and recovery
+	// gin.New() creates a clean router with no middleware.
+
+	router.Use(gin.Logger()) // adds middleware to the router.
+	// | Part           | Meaning                                       |
+	// | -------------- | --------------------------------------------- |
+	// | `router.Use()` | Adds middleware to router                     |
+	// | `gin.Logger()` | Built-in middleware for logging HTTP requests |
+	// gin.Logger()	// logs every HTTP request to the console.
+
+	routes.AuthRoutes()
+	routes.UserRoutes()
+
+	router.GET("/api-1", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{"success": "Access granted for api-1"})
+	})
+
+	router.GET("/api-2", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{"success": "Access granted for api-2"})
+	})
+
+	router.Run(":" + port)
+}
---
+++ models/userModel.go
@@ -0,0 +1,18 @@
+package models
+
+import "time"
+
+type user struct {
+	Id            int       `json:"id"`
+	FirstName     string    `json:"first_name" validate:"required, min=2, max=100"`
+	LastName      string    `json:"last_name" validate:"required, min=2, max=100"`
+	Password      string    `json:"password" validate:"required, min=6"`
+	Email         string    `json:"email" validate:"required, email"`
+	Phone         string    `json:"phone" validate:"required"`
+	Token         string    `json:"token"`
+	UserType      string    `json:"user_type" validate:"required, eq=ADMIN|eq=USER"`
+	RefereshToken string    `json:"referesh_token"`
+	CreatedAt     time.Time `json:"created_at"`
+	UpdatedAt     time.Time `json:"updated_at"`
+	UserId        string    `json:"user_id"`
+}
